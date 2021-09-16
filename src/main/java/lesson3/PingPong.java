189947bf6788df0acad6f0328cde140eb2005284
MarkSemenov <markswell@mail.ru>
2021-09-16 18:11:07 +0300

    homework

---
+++ src/main/java/lesson3/PingPong.java
@@ -0,0 +1,42 @@
+package lesson3;
+
+public class PingPong {
+    static final Object o = new Object();
+
+    public static void main(String[] args) {
+        new Thread(PingPong::ping).start();
+        new Thread(PingPong::pong).start();
+    }
+
+    public static void ping() {
+        synchronized (o) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Ping");
+                o.notify();
+                try {
+                    o.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    o.notify();
+                }
+            }
+        }
+    }
+
+
+    public static void pong() {
+        synchronized (o) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Pong");
+                o.notify();
+                try {
+                    o.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
